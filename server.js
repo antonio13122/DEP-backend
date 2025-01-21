a9df0601c9a1a043d8031ff34d1a0434fec41720
Antonio <tonisajbic501@gmail.com>
2025-01-21 18:13:56 +0100

    changed iins

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-// Initialize Express app
 const app = express();
 
 // Middleware
@@ -11,7 +10,7 @@ app.use(express.json());
 
 // MongoDB Connection
 mongoose
-  .connect("mongodb://localhost:27017/mydatabase") // Removed deprecated options
+  .connect("mongodb://localhost:27017/mydatabase")
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
@@ -20,11 +19,9 @@ app.get("/", (req, res) => {
   res.send("Welcome to the API!");
 });
 
-// Import and Use Routes
-const userRoutes = require("./routes/userRoutes"); // Path to your router file
-app.use("/api/users", userRoutes); // Connects /api/users endpoint to the router
+const userRoutes = require("./routes/userRoutes");
+app.use("/api/users", userRoutes);
 
-// Start the Server
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
