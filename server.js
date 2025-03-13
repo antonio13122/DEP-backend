be826f9f9b0871fcb99c35181a554c49c5dea1d5
Antonio <tonisajbic501@gmail.com>
2025-03-13 17:24:03 +0100

    packing and env

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.get("/", (req, res) => {
 const userRoutes = require("./routes/userRoutes");
 app.use("/api/users", userRoutes);
 
-const PORT = 5000;
+const PORT = process.env.Port || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
