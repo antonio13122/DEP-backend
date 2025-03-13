b12af52af9b7f81af588ddf77d5101251cbeefea
Antonio <tonisajbic501@gmail.com>
2025-03-13 18:02:22 +0100

    Fix MongoDB connection & Heroku port issue

--- src/server.js
+++ src/server.js
@@ -39,7 +39,7 @@ app.get("/", (req, res) => {
 const userRoutes = require("../routes/userRoutes.js");
 app.use("/api/users", userRoutes);
 
-const PORT = process.env.Port || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
